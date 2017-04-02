22f4b30e82accd7b427b29d370d8ca1726029299
vidyasagarv <vidyasagar.039@gmail.com>
2017-04-01 23:54:12 -0400

    add MergeTwoLists problem

--- LinkedList/MergeTwoLists.java
+++ LinkedList/MergeTwoLists.java
@@ -6,8 +6,7 @@
  *     ListNode(int x) { val = x; }
  * }
  */
-public class MergeTwoLists {
-    
+public class MergeTwoLists {   
     public static ListNode mergeTwoLists(ListNode l1, ListNode l2) {
         if(l1==null)
         return l2;
