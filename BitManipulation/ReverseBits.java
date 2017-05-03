e6003ce617acfed8cc5d8f299314c90676df44f0
vidyasagarv <vidyasagar.039@gmail.com>
2017-05-02 23:53:13 -0400

    add ReverseBits problem

---
+++ BitManipulation/ReverseBits.java
@@ -0,0 +1,21 @@
+public class ReverseBits {
+    // you need treat n as an unsigned value
+    public int reverseBits(int n) {
+        for(int i = 0; i < 16; ++i){
+            n = swapBits(n, i, 32-i-1);
+        }
+        return n;
+    }
+    
+    //swap bits
+    public int swapBits(int n, int i, int j){
+        int a = (n >> i) & 1;
+        int b = (n >> j) & 1;
+        
+        // if bits are different then swap
+        if((a ^ b) != 0){
+            return n ^= (1 << i) | (1 << j);
+        }
+        return n;
+    }
+}
