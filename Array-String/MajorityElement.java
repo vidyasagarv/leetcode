003939878c40619fc02ab94e5553c335adb2063b
vidyasagarv <vidyasagar.039@gmail.com>
2017-05-09 17:21:09 -0400

    add MajorityElement problem

---
+++ Array-String/MajorityElement.java
@@ -0,0 +1,18 @@
+public class MajorityElement {
+    public int majorityElement(int[] nums) {
+    	// more intuitive solution is to sort the array
+    	// and return the mid element
+        int result = 0, count = 0;
+        for(int i = 0; i < nums.length; i++){
+            if(count == 0){
+                result = nums[i];
+                count = 1;
+            } else if(result == nums[i]){
+                count++;
+            } else{
+                count--;
+            }
+        }
+        return result;
+    }
+}
