a81dfb6eb3f2f3aac41bdf1a1c64f0aa2a408e93
vidyasagarv <vidyasagar.039@gmail.com>
2017-05-13 15:38:59 -0400

    add BestTimeBuySellStock2 problem

---
+++ DynamicProgramming/BestTimeBuySellStock2.java
@@ -0,0 +1,12 @@
+public class BestTimeBuySellStock2 {
+    public int maxProfit(int[] prices) {
+        int maxProfit = 0;
+        
+        for(int i = 0; i < prices.length-1; i++){
+            if(prices[i+1] > prices[i]){
+                maxProfit += prices[i+1] - prices[i];
+            }
+        }
+        return maxProfit;
+    }
+}
