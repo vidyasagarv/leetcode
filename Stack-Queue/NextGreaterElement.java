8365902f5a6b07d21ec71fa5691702e31a77d18b
vidyasagarv <vidyasagar.039@gmail.com>
2017-05-20 22:58:54 -0400

    update NextGreaterElement problem

--- Stack-Queue/NextGreaterElement.java
+++ Stack-Queue/NextGreaterElement.java
@@ -1,14 +1,20 @@
 public class NextGreaterElement {
     public int[] nextGreaterElement(int[] findNums, int[] nums) {
-        Map<Integer, Integer> nextGreaterElem = new HashMap<>();
+        Map<Integer, Integer> nextGreaterInt = new HashMap<>();
         Stack<Integer> stack = new Stack<>();
-        for (int num : nums) {
-            while (!stack.isEmpty() && stack.peek() < num)
-                nextGreaterElem.put(stack.pop(), num);
+        for(int num : nums){
+            while(!stack.isEmpty() && stack.peek() < num){
+                nextGreaterInt.put(stack.pop(), num);
+            }
             stack.push(num);
-        }   
-        for (int i = 0; i < findNums.length; i++)
-            findNums[i] = nextGreaterElem.getOrDefault(findNums[i], -1);
+        }
+        for(int i = 0; i < findNums.length; i++){
+            if(nextGreaterInt.get(findNums[i]) != null){
+                findNums[i] = nextGreaterInt.get(findNums[i]);
+            } else{
+                findNums[i] = -1;
+            }
+        }
         return findNums;
     }
 }
