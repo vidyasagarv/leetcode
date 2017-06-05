7db0eec18480f269aaf3e32fd67d007d0e229ad3
vidyasagarv <vidyasagar.039@gmail.com>
2017-06-04 22:59:41 -0400

    add HammingDistance problem

---
+++ BitManipulation/HammingDistance.java
@@ -0,0 +1,10 @@
+public class HammingDistance {
+    public int hammingDistance(int x, int y) {
+        int xor = x ^ y;
+        int distance = 0;
+        for(int i = 0; i < 32; i++){
+            distance += (xor >> i) & 1;
+        }
+        return distance;
+    }
+}
